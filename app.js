138174507b65a22929114178289bb5374def1dbb
Dan Quintal LT <danjquintal@gmail.com>
2025-11-14 12:32:55 +0000

    Aula 9

--- app.js
+++ app.js
@@ -1,41 +1,96 @@
-//13/11/2025 Aula 8
+//14/11/2025 Aula 8
+//Des 5
+
+let estrela = "*";
+let numeroEstrelas = 4;
+while (numeroEstrelas > 0) {
+  console.log(estrela);
+  estrela += "*";
+  numeroEstrelas--;
+}
+
+//Extra (Árvore de Natal)
+
+let estrelas = "*";
+let numEstrelas = 15;
+let espacos = " ";
+let base = numEstrelas / 2 + numEstrelas / 3;
+
+while (numEstrelas > 0) {
+  console.log(`${espacos.repeat(numEstrelas)}${estrelas}`);
+  estrelas += "**";
+  numEstrelas--;
+  if (numEstrelas == 0) {
+    console.log(`${espacos.repeat(base)}|   |`);
+    console.log(`${espacos.repeat(base)}|   |`);
+  }
+}
+
+//14/11/2025 Aula 9
 //Des 1
 
-let saldo = 0;
-const LIMITE = 50;
-const VALOR_DEPOSITO = 10;
+let ciclo = 10;
+let somaPares = 0;
+let listaPares = "";
 
-while(saldo < LIMITE){
-    saldo+=VALOR_DEPOSITO;
+while (ciclo > 0) {
+  if (ciclo % 2 == 0) {
+    somaPares += ciclo;
+    listaPares += ciclo + "  ";
+  }
+  ciclo--;
 }
-console.log(`Saldo atingido ou ultrapassado. Saldo atual = ${saldo}`)
+console.log(`Os números pares são:  ${listaPares},e a sua soma é ${somaPares}`);
 
 //Des 2
 
-let cont = 10;
-while(cont > 0){
-    if(cont % 3 == 0){
-        console.log(String(cont),"é divisível por 3!")
-    }else{
-        console.log(String(cont));        
-    }
-    cont--;
+const LIMITE = 10;
+let cicloIdade = 10;
+let idade = 15;
+let verificaIdade = null;
+
+while (cicloIdade > 0) {
+  verificaIdade = idade > 20
+      ? console.log(idade," Adulto Plus")
+      : idade >= 18
+      ? console.log(idade, " Adulto Jovem")
+      : console.log(idade, " Menor");
+  idade++;
+  cicloIdade--;
 }
 
 //Des 3
 
-let somaNumerosPositivos = 0;
-let valor = 0;
-do{
-    valor = Number(prompt("Insira um número positivo", 0))
-    somaNumerosPositivos += valor;
-}while(valor > 0);
-console.log(`O último valor inserido não é positivo, o total da soma é ${somaNumerosPositivos-valor}`)
+let fatorial = 1;
+const N = 5;
+for(let i = 1; i <= N; i++){
+  fatorial *= i
+}
+console.log(fatorial)
 
 //Des 4
 
-for(let i = 1; i <= 5; i++){
-    for(let a = 1;a <= 10; a++){
-        console.log(`${i} x ${a} = ${i*a}`)
-    }
+const senha = "abc";
+let tentativaAtual = String(prompt("Insira a pw."))
+while(true){
+  if(senha === tentativaAtual){
+    console.log("Pw correta, user logado.")
+    break;
+  }else{
+    tentativaAtual = String(prompt("Senha incorreta, tente novamente."))
+  }
 }
+console.log("While encerrado.")
+
+//Des 5
+
+for(let i = 1; i <= 20; i++){
+  if(i%5 != 0){
+    continue;
+  }else{
+    console.log("Múltiplo de 5 ->", String(i));
+  }
+}
+
+//Des 6
+
--- old.js
+++ old.js
@@ -51,3 +51,34 @@ const VALOR_DEPOSITO = 10;
 while(saldo < LIMITE){
     saldo+=VALOR_DEPOSITO;
 }
+console.log(`Saldo atingido ou ultrapassado. Saldo atual = ${saldo}`)
+
+//Des 2
+
+let cont = 10;
+while(cont > 0){
+    if(cont % 3 == 0){
+        console.log(String(cont),"é divisível por 3!")
+    }else{
+        console.log(String(cont));        
+    }
+    cont--;
+}
+
+//Des 3
+
+let somaNumerosPositivos = 0;
+let valor = 0;
+do{
+    valor = Number(prompt("Insira um número positivo", 0))
+    somaNumerosPositivos += valor;
+}while(valor > 0);
+console.log(`O último valor inserido não é positivo, o total da soma é ${somaNumerosPositivos-valor}`)
+
+//Des 4
+
+for(let i = 1; i <= 5; i++){
+    for(let a = 1;a <= 10; a++){
+        console.log(`${i} x ${a} = ${i*a}`)
+    }
+}
