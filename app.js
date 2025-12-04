abdc20f60473f4f8cab0be978f4a46c2325b21f9
Dan Quintal LT <danjquintal@gmail.com>
2025-12-04 09:18:19 +0000

    03/12/2025

---
+++ app.js
@@ -0,0 +1,27 @@
+//M1 Dia 21: Debugging e Erros
+
+//Parte 2: Interpretação e Console
+
+//Identificação de Erros
+
+//1 falta o ) - Syntax
+//2 Syntax, Upper num numero
+//3 variavel não declarada - Reference
+//4 alterar uma const - Type
+//5 Parte 2: Interpretação e Console - Syntax, funcão sem nome
+
+//Previsão de Debug
+
+//6 False
+//7 Aparece a mensagem 'Entrou' porque x foi igualado a 10, deveria ser ==
+//8 Out of range , undefined, a lista apenas contem dois elementos
+//9 a propriedade nome não existe
+//10 Nan, o segundo valor é uma string
+
+/*Ferramentas do Console
+
+console.warn("Bateria Fraca")
+console.error("Falha no sistema")
+console.table([{id:1, nome:"Ana"}, {id:2, nome:"Rui"}])
+console.clear()
+console.log(typeof temp)*/
--- old.js
+++ old.js
@@ -594,7 +594,7 @@ for(let i = 0; i < frutas.length; i++){
 
 //Exe Extra
 
-const numeros = [1,2,3];
+//const numeros = [1,2,3];
 for(let i = 0; i < numeros.length; i++){
     numeros[i] = numeros[i] * 2;
     console.log(`${numeros[i]}`)
@@ -737,7 +737,7 @@ function imprimeArrayForEach(){
 };
 
 //Exe 6
-const numeros = [1, 2, 3, 4, 5];
+//const numeros = [1, 2, 3, 4, 5];
 
 function quadrado(array){
     const quadrados = array.map(n => n ** 2);
@@ -813,3 +813,32 @@ function somaIdades(){
 };
 
 //somaIdades()
+
+
+//M1 Dia 21: Debugging e Erros
+
+//Parte 2: Interpretação e Console
+
+//Identificação de Erros
+
+//1 falta o ) - Syntax
+//2 Syntax, Upper num numero
+//3 variavel não declarada - Reference
+//4 alterar uma const - Type
+//5 Parte 2: Interpretação e Console - Syntax, funcão sem nome
+
+//Previsão de Debug
+
+//6 False
+//7 Aparece a mensagem 'Entrou' porque x foi igualado a 10, deveria ser ==
+//8 Out of range , undefined, a lista apenas contem dois elementos
+//9 a propriedade nome não existe
+//10 Nan, o segundo valor é uma string
+
+/*Ferramentas do Console
+
+console.warn("Bateria Fraca")
+console.error("Falha no sistema")
+console.table([{id:1, nome:"Ana"}, {id:2, nome:"Rui"}])
+console.clear()
+console.log(typeof temp)*/
